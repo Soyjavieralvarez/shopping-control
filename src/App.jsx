dcf24ef4e00426547c23fa5b5ce522efce401c22
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-04-03 17:30:00 +0200

    first steps with modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import Header from './components/Header'
-
+import newExpenseIcon from './assets/img/nuevo-gasto.svg'
 
 function App() {
   const [budget, setBudget] = useState(0);
   const [isValidBudget, setIsValidBudget] = useState(false)
+  const [modal, setModal] = useState(false)
 
+  const handleNewExpense = () => {
+    setModal(true)
+  }
   return (
    <div>
     <Header
@@ -14,6 +18,19 @@ function App() {
     isValidBudget={isValidBudget}
     setIsValidBudget={setIsValidBudget}
     />
+
+
+     {isValidBudget && (
+    <div className='nuevo-gasto'>
+      <img
+      src={newExpenseIcon}
+      alt="icono de nuevo gasto"
+      onClick={handleNewExpense}
+       />
+    </div>
+    )}
+
+    {modal && <p>Desde modal</p>}
    </div>
   )
 }
